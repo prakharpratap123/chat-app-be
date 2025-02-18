3513d394d2bef6dbc2d496d0167b56f2ceca858b
prakharpratap123 <pratapprakhar111@gmail.com>
2025-02-18 19:18:21 +0530

    cors error fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -10,6 +11,12 @@ const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
